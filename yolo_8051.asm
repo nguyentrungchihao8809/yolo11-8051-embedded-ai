9a5892d3c20d435615effe32b413cc758b8b0031
KinnDuyen <ttkduyen040305@gmail.com>
2026-04-11 23:05:00 +0700

    implement Timer0 ISR for countdown and auto-off logic
    Added Timer 0 Interrupt Service Routine (ISR) for handling timeout.

--- yolo_8051.asm
+++ yolo_8051.asm
@@ -1,26 +1,42 @@
 $INCLUDE (REG51.INC)
+LED      BIT P1.0
+BUZZER   BIT P1.1
+TIMEOUT  EQU 30H
 
-LED      BIT P1.0   
-BUZZER   BIT P1.1   
-TIMEOUT  EQU 30H    
-
-ORG 0000H          
+ORG 0000H
 LJMP MAIN
 
-ORG 0100H            
+ORG 000BH           
+LJMP T0_ISR          
+
+ORG 0100H
 MAIN:
     MOV SP, #60H
-
     MOV TMOD, #21H  
-    MOV TH0, #3CH    
-    MOV TL0, #0B0H  
+    MOV TH0, #3CH   
+    MOV TL0, #0B0H
     MOV TIMEOUT, #0  
-    CLR LED         
-    CLR BUZZER      
-
+    CLR LED
+    CLR BUZZER
     MOV TH1, #0FDH   
     MOV SCON, #50H  
-    SETB TR1       
+    SETB TR1         
 
-    SJMP $      
+    SJMP $           
+
+T0_ISR:
+    MOV TH0, #3CH    
+    MOV TL0, #0B0H
+    MOV A, TIMEOUT   
+    JZ EXIT_T0       
+    
+    DEC TIMEOUT      
+    MOV A, TIMEOUT
+    JNZ EXIT_T0     
+    
+    CLR LED          
+    CLR BUZZER
+EXIT_T0:
+    RETI            
+
 END
