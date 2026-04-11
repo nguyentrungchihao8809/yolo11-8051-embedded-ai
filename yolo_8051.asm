d29050863b202f01bd0c508cb65df415481abb39
Nguyễn Trung Chí Hào <nguyentrungchihao8809@gmail.com>
2026-04-11 13:19:30 +0700

    Update yolo_8051.asm

--- yolo_8051.asm
+++ yolo_8051.asm
@@ -1,22 +1,26 @@
 $INCLUDE (REG51.INC)
 
-LED      BIT P1.0    
+LED      BIT P1.0   
 BUZZER   BIT P1.1   
 TIMEOUT  EQU 30H    
 
-ORG 0000H           
+ORG 0000H          
 LJMP MAIN
 
-ORG 0100H           
+ORG 0100H            
 MAIN:
-    MOV SP, #60H     
+    MOV SP, #60H
 
-    MOV TMOD, #21H   
-    MOV TH0, #3CH   
+    MOV TMOD, #21H  
+    MOV TH0, #3CH    
     MOV TL0, #0B0H  
     MOV TIMEOUT, #0  
     CLR LED         
     CLR BUZZER      
 
-    SJMP $          
+    MOV TH1, #0FDH   
+    MOV SCON, #50H  
+    SETB TR1       
+
+    SJMP $      
 END
