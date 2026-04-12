7a8e1349ba2d53d181f2ce95ced462f7af309529
Nguyễn Trung Chí Hào <nguyentrungchihao8809@gmail.com>
2026-04-12 15:27:07 +0700

    Update yolo_8051.asm
    feat: implement UART ISR to trigger devices on char 'A'

--- yolo_8051.asm
+++ yolo_8051.asm
@@ -6,37 +6,52 @@ TIMEOUT  EQU 30H
 ORG 0000H
 LJMP MAIN
 
-ORG 000BH           
-LJMP T0_ISR          
+ORG 000BH
+LJMP T0_ISR
+
+ORG 0023H            
+LJMP UART_ISR       
 
 ORG 0100H
 MAIN:
+    
     MOV SP, #60H
-    MOV TMOD, #21H  
-    MOV TH0, #3CH   
+    MOV TMOD, #21H
+    MOV TH0, #3CH
     MOV TL0, #0B0H
-    MOV TIMEOUT, #0  
+    MOV TIMEOUT, #0
     CLR LED
     CLR BUZZER
-    MOV TH1, #0FDH   
-    MOV SCON, #50H  
-    SETB TR1         
+    MOV TH1, #0FDH
+    MOV SCON, #50H
+    SETB TR1
 
     SJMP $           
 
+UART_ISR:
+    JNB RI, EXIT_UART     
+    MOV A, SBUF           
+    CLR RI                
+    CJNE A, #'A', EXIT_UART 
+    
+    SETB LED              
+    SETB BUZZER           
+    MOV TIMEOUT, #20      
+
+EXIT_UART:
+    RETI
+
 T0_ISR:
-    MOV TH0, #3CH    
+    MOV TH0, #3CH
     MOV TL0, #0B0H
-    MOV A, TIMEOUT   
-    JZ EXIT_T0       
-    
-    DEC TIMEOUT      
     MOV A, TIMEOUT
-    JNZ EXIT_T0     
-    
-    CLR LED          
+    JZ EXIT_T0
+    DEC TIMEOUT
+    MOV A, TIMEOUT
+    JNZ EXIT_T0
+    CLR LED
     CLR BUZZER
 EXIT_T0:
-    RETI            
+    RETI
 
 END
